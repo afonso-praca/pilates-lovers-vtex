bb27c0b740482a3a36ac404089c3f1ac1110370e
afonso <afonsoinfo@gmail.com>
2016-11-21 09:57:38 -0200

    adds script to show logout btn

--- src/arquivos/plt-custom-scripts.js
+++ src/arquivos/plt-custom-scripts.js
@@ -1,10 +1,29 @@
-$("#slick-container > div:gt(0)").hide();
+(function () {
 
-setInterval(function() {
-  $('#slick-container > div:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#slick-container');
-},  4000);
+  var startSlicker = function () {
+    $("#slick-container > div:gt(0)").hide();
+    setInterval(function() {
+      $('#slick-container > div:first')
+        .fadeOut(1000)
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo('#slick-container');
+    },  4000);
+  };
+
+  var verifyUserLoggin = function () {
+    $(window).on('orderFormUpdated.vtex', function () {
+      if (vtexjs.checkout.orderForm.loggedIn){
+        $('.btn-login').remove();
+        $('.link-login').remove();
+        $('.btn-logout').removeClass('hidden');
+        $('.link-logout').removeClass('hidden');
+      }
+    });
+  };
+
+  verifyUserLoggin();
+  startSlicker()
+
+})();
